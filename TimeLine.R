64446a3e484a81012763224575c6b9307a459b83
Alejandro Yáñez Rubio <aleyanez.rubio@gmail.com>
2020-05-21 13:58:37 -0400

    2nd type ready

--- TimeLine.R
+++ TimeLine.R
@@ -4,34 +4,35 @@ yr <- c(1985:2019)
 #y_axis <- c('Desembarque', 'CPUE', 'Cruceros')
 
 LAmNorte <- ggplot(NULL) + 
-  annotate("rect", xmin=1985, xmax=2019, ymin=14, ymax=15, alpha=.8, fill="red2") + #desembarque
+  annotate("rect", xmin=1984.5, xmax=2019.5, ymin=14, ymax=15, alpha=.8, fill="red2") + #desembarque
   annotate("text", x=2000, y=14.5, label="Desembarque", colour="white") +
   
-  annotate("rect", xmin=1987, xmax=1991, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 1
-  annotate("rect", xmin=1993, xmax=2009, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 2
-  annotate("rect", xmin=2012, xmax=2012.5, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 3
+  annotate("rect", xmin=1986.5, xmax=1991.5, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 1
+  annotate("rect", xmin=1992.5, xmax=2009.5, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 2
+  annotate("rect", xmin=2011.5, xmax=2012.5, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 3
   #annotate("segment", x=2012, xend=2012, y=12, yend=13, colour="dodgerblue2") +            # cpue bloq 3
-  annotate("rect", xmin=2015, xmax=2019, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 4
+  annotate("rect", xmin=2014.5, xmax=2019.5, ymin=12, ymax=13, alpha=.8, fill="dodgerblue2") + # cpue bloq 4 
   annotate("text", x=2000, y=12.5, label="CPUE", colour="white") +
   
 #  annotate("segment", x=1997, xend=1997, y=10, yend=11, colour="green4") +            # crucero bloq 1
-  annotate("rect", xmin=1997, xmax=1997.5, ymin=10, ymax=11, alpha=.8, fill="green4") + # crucero bloq 1
-  annotate("rect", xmin=1999, xmax=2009, ymin=10, ymax=11, alpha=.8, fill="green4") + # crucero bloq 2
-  annotate("rect", xmin=2011, xmax=2019, ymin=10, ymax=11, alpha=.8, fill="green4") +  # crucero bloq 3
+  annotate("rect", xmin=1996.5, xmax=1997.5, ymin=10, ymax=11, alpha=.8, fill="green4") + # crucero bloq 1
+  annotate("rect", xmin=1998.5, xmax=2009.5, ymin=10, ymax=11, alpha=.8, fill="green4") + # crucero bloq 2
+  annotate("rect", xmin=2010.5, xmax=2019.5, ymin=10, ymax=11, alpha=.8, fill="green4") +  # crucero bloq 3
   annotate("text", x=2004, y=10.5, label="Biomasa Crucero", colour="white") +
   
-  annotate("rect", xmin=1993, xmax=2019, ymin=8, ymax=9, alpha=.8, fill="darkorchid2") + # prop hembras
-  annotate("rect", xmin=1985, xmax=1993, ymin=8, ymax=9, alpha=.8, fill="darkorchid4") + # prop hembras
+  annotate("rect", xmin=1992.5, xmax=2019.5, ymin=8, ymax=9, alpha=.8, fill="darkorchid2") + # prop hembras
+  annotate("rect", xmin=1984.5, xmax=1992.5, ymin=8, ymax=9, alpha=.8, fill="darkorchid4") + # prop hembras
   annotate("text", x=2003, y=8.5, label="Prop Hembras", colour="white") +
   
-  annotate("rect", xmin=1985, xmax=1986, ymin=6, ymax=7, alpha=.8, fill="coral1") +  # ET Flota
-  annotate("rect", xmin=1993, xmax=2012, ymin=6, ymax=7, alpha=.8, fill="coral1") + # ET Flota
-  annotate("rect", xmin=2014, xmax=2019, ymin=6, ymax=7, alpha=.8, fill="coral1") +# ET Flota
+  annotate("rect", xmin=1984.5, xmax=1986.5, ymin=6, ymax=7, alpha=.8, fill="coral1") +  # ET Flota
+  annotate("rect", xmin=1992.5, xmax=2012.5, ymin=6, ymax=7, alpha=.8, fill="coral1") + # ET Flota
+  annotate("rect", xmin=2013.5, xmax=2019.5, ymin=6, ymax=7, alpha=.8, fill="coral1") +# ET Flota
   annotate("text", x=2003, y=6.5, label="ET Flota", colour="white") +
   
-  annotate("segment", x=1999, xend=1999, y=4, yend=5, colour="gold2") +          # ET Crucero
-  annotate("rect", xmin=2001, xmax=2009, ymin=4, ymax=5, alpha=.8, fill="gold2") +  # ET Crucero
-  annotate("rect", xmin=2011, xmax=2019, ymin=4, ymax=5, alpha=.8, fill="gold2") +  # ET Crucero
+  #annotate("segment", x=1999, xend=1999, y=4, yend=5, colour="gold2") +          # ET Crucero
+  annotate("rect", xmin=1998.5, xmax=1999.5, ymin=4, ymax=5, alpha=.8, fill="gold2") +          # ET Crucero
+  annotate("rect", xmin=2000.5, xmax=2009.5, ymin=4, ymax=5, alpha=.8, fill="gold2") +  # ET Crucero
+  annotate("rect", xmin=2010.5, xmax=2019.5, ymin=4, ymax=5, alpha=.8, fill="gold2") +  # ET Crucero
   annotate("text", x=2005, y=4.5, label="ET Crucero", colour="white") +
   
   annotate("rect", xmin=2011, xmax=2011, ymin=3, ymax=16, alpha=.8, fill="grey")  + # Espacio
