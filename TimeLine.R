68ebd4289e3d870fb3977b1b3aef2dab69a99b3e
Alejandro Yáñez Rubio <aleyanez.rubio@gmail.com>
2020-05-21 01:24:26 -0400

    Update TimeLine.R
    reemplazo por algunos de mis datos

--- TimeLine.R
+++ TimeLine.R
@@ -1,33 +1,34 @@
 library(vistime)
 
 LAmNorte.data <- data.frame(
-  DeviceName = c("Cypher Sirolimus DES", "Taxus Express 2", "Cypher Select Sirolimus DES",
-                 "Cypher Select Plus Sirolimus DES", "Taxus Liberte", "Endeavor ABT578",
-                 "Endeavor Sprint Zotarolimus DES", "Xience V", "Taxus Element Monrail ION",
-                 "Xience Nano", "Promus Element Plus", "Xience Prime",
-                 "Endeavor Resolute DES","Endeavor Resolute Integrity DES", "Promus Premier", "Xience Xpedition LL and SV"),
-  DeviceManufacturer = c("Cordis Cashel","Boston Scientific","Cordis Cashel",
-                         "Cordis Cashel","Boston Scientific","Medtronic Inc",
-                         "Medtronic Inc", "Abbott Vascular", "Boston Scientific",
-                         "Abbott Vascular","Boston Scientific", "Abbott Vascular",
-                         "Medtronic Inc", "Medtronic Inc","Boston Scientific", "Abbott Vascular"),
-  start_date = as.Date(c("2000-01-01", "2006-01-01", "2019-01-01", 
-                         "2006-10-25","2008-02-05", "2008-02-27",
-                         "2009-06-10", "2009-08-21", "2011-08-19",
-                         "2011-10-24", "2012-01-30", "2012-04-10",
-                         "2012-04-14", "2013-03-07", "2013-09-30", "2014-02-19")),
-  end_date = as.Date(c("2019-07-18", "2010-11-10", "2007-07-18",
-                       "2013-04-05", "2013-11-01", "2016-03-31",
-                       "2016-03-31", "2016-03-31", "2011-09-16",
-                       "2016-03-31", "2016-03-31", "2016-03-31",
-                       "2016-03-31", "2016-03-31", "2016-03-31", "2016-03-31")),
+  DatosEntrada = c("Desembarques", 
+                 "CPUE", 
+                 "Ev. Directa Cruceros", 
+                 "Xience Prime"),
+  
+  DeviceManufacturer = c("Cordis Cashel",
+                         "Cordis Cashel",
+                         "Cordis Cashel", 
+                         "Abbott Vascular"),
+  
+  start_date = as.Date(c("1985-01-01", 
+                         "1987-01-01",
+                         "1997-01-01", 
+                         "2012-04-10")),
+  
+  end_date = as.Date(c("2019-01-01", 
+                       "2019-01-01", 
+                       "2019-01-01", 
+                       "2016-03-31")),
+  
   stringsAsFactors = T
 )
 
 #data visualization
-vistime(device.data, events = "DeviceName",
+vistime(LAmNorte.data, events = "DatosEntrada", groups = "DeviceManufacturer",
         start = "start_date", end = "end_date")
 
+?vistime
 
 timeline(device.data)
 
